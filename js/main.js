2622b136790b7bf4b8e95ec62777598469d35af2
i-carrie <50307972+i-carrie@users.noreply.github.com>
2022-12-31 16:31:46 +0900

    create menu

--- js/main.js
+++ js/main.js
@@ -34,3 +34,24 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+
+$(function(){
+  $('.toggle_btn').on('click', function() {
+    if ($('#header').hasClass('open')) {
+      $('#header').removeClass('open');
+    } else {
+      $('#header').addClass('open');
+    }
+  });
+
+  // #maskのエリアをクリックした時にメニューを閉じる
+  $('#mask').on('click', function() {
+    $('#header').removeClass('open');
+  });
+
+  // リンクをクリックした時にメニューを閉じる
+  $('#navi a').on('click', function() {
+    $('#header').removeClass('open');
+  });
+});
